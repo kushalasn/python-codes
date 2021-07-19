426be6967d2906e3558da8f994e4001801855673
Kushal Asn <47448959+kushalasn@users.noreply.github.com>
2021-07-19 15:47:42 +0530

    Update roman_integer.py

--- roman_integer.py
+++ roman_integer.py
@@ -1,25 +1,13 @@
 class Solution:
-    def romanToInt(self, str):
-        set={'I' :1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        l=len(str)
-        count=0
+    def romanToInt(self, s) :
+        d={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        res=0
         i=0
-        while (i <len(str)):
-            c1=set[str[i]]
-            if (i+1<len(str)):
-                
-                c2=set[str[i+1]]
-                if (c1<c2):
-                    count=count-c1
-                    i=i+1
-                     
-                else:
-                    count=count+c1
-                    i=i+1
-                    
+        while(i<len(s)):
+            if ((i+1)<len(s) and (d[s[i]]<d[s[i+1]])):
+                res+=d[s[i+1]]-d[s[i]]
+                i+=2
             else:
-                count=count+c1
-                i=i+1
-           
-                
-        return count       
+                res+=d[s[i]]
+                i+=1
+        return res        
